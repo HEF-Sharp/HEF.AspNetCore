6964a763a03e77a9fd535e3b0ead004bda24be0e
万里涛 <wanlitao_3055601@outlook.com>
2023-12-26 18:20:29 +0800

    add hef http result

---
+++ src/HEF.AspNetCore.Mvc/Extensions/HEFDoResultToHttpExtensions.cs
@@ -0,0 +1,54 @@
+﻿using HEF.Core;
+using System;
+using System.Net;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    public static class HEFDoResultToHttpExtensions
+    {
+        public static HEFHttpResult ToHttpResult(this HEFDoResult doResult)
+        {
+            if (doResult == null)
+                throw new ArgumentNullException(nameof(doResult));
+
+            return new HEFHttpResult
+            {
+                Code = DoResultTypeToHttpCode(doResult.Type),
+                Msg = doResult.Msg
+            };
+        }
+
+        public static HEFHttpResult<TResultData> ToHttpResult<TResultData>(this HEFDoResult<TResultData> doResult)
+        {
+            if (doResult == null)
+                throw new ArgumentNullException(nameof(doResult));
+
+            return new HEFHttpResult<TResultData>
+            {
+                Code = DoResultTypeToHttpCode(doResult.Type),
+                Msg = doResult.Msg,
+                Data = doResult.Data
+            };
+        }
+
+        private static int DoResultTypeToHttpCode(string doResultTypeStr)
+        {
+            if (string.IsNullOrWhiteSpace(doResultTypeStr))
+                return 0;
+
+            if (Enum.TryParse<HEFDoResultType>(doResultTypeStr, true, out var doResultType))
+            {
+                return doResultType switch
+                {
+                    HEFDoResultType.success => (int)HttpStatusCode.OK,
+                    HEFDoResultType.fail => (int)HttpStatusCode.InternalServerError,
+                    HEFDoResultType.validFail => (int)HttpStatusCode.BadRequest,
+                    HEFDoResultType.notFound => (int)HttpStatusCode.NotFound,
+                    _ => 0
+                };
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ src/HEF.AspNetCore.Mvc/Result/HEFHttpResult.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    public class HEFHttpResult
+    {
+        public int Code { get; set; }
+
+        public string Msg { get; set; }
+
+        public bool Success => Code == (int)HttpStatusCode.OK;
+    }
+
+    public class HEFHttpResult<TResultData> : HEFHttpResult
+    {
+        public TResultData Data { get; set; }
+    }
+}
